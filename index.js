1840ff470ea6c40c65f7cab13d499e2fa7b7552b
fmwong77 <fongmeiw@gmail.com>
2019-12-08 08:04:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names)
+{ 
+  let thank = []
+  let i = 0;
+  while (i <= names.length - 1) {
+    thank.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    i++;
+  }
+
+  return thank;
+}
+
+function countDown(number)
+{
+  while (number >= 0)
+  {
+    console.log(number);
+    number -= 1;
+  }
+}
